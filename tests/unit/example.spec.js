ca6e38276822c231e785ade6c5a665d810097b9b
Jose Gabriel Gonzalez <gabriel.gonzalez@morningstar.com>
2020-05-14 00:14:14 -0500

    Removing HelloWorld Component

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import DevelopersList from '@/components/DevelopersList.vue';
 
-describe('HelloWorld.vue', () => {
+describe('DevelopersList.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(DevelopersList, {
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
